5cb263485321450d3f830ab6383d107a78f10641
AugustoNGonzalez <129183525+AugustoNGonzalez@users.noreply.github.com>
2025-12-07 18:17:36 -0300

    Agregadas ID

---
+++ Front/display.js
@@ -0,0 +1,7 @@
+const mainTable = document.getElementById("mainTbl");
+const resetBtn = document.getElementById("resetBtn");
+const addBtn = document.getElementById("addBtn");
+const addGroupBtn = document.getElementById("addGroupBtn");
+const mostUsedTable = document.getElementById("mostUsed");
+const mostUsedBody = document.getElementById("mostUsedBody");
+
